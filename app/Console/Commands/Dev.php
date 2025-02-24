62c43de3c7c13af54d4d47d16792512a4261d039
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-02-24 18:23:44 +0100

    chore(commands): remove unused code from dev:init command

--- app/Console/Commands/Dev.php
+++ app/Console/Commands/Dev.php
@@ -5,58 +5,16 @@ namespace App\Console\Commands;
 use App\Models\InstanceSettings;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Process;
-use Symfony\Component\Yaml\Yaml;
 
 class Dev extends Command
 {
-    protected $signature = 'dev {--init} {--generate-openapi}';
+    protected $signature = 'dev:init';
 
-    protected $description = 'Helper commands for development.';
+    protected $description = 'Initialize the development environment.';
 
     public function handle()
     {
-        if ($this->option('init')) {
-            $this->init();
-
-            return;
-        }
-        if ($this->option('generate-openapi')) {
-            $this->generateOpenApi();
-
-            return;
-        }
-    }
-
-    public function generateOpenApi()
-    {
-        // Generate OpenAPI documentation
-        echo "Generating OpenAPI documentation.\n";
-        // https://github.com/OAI/OpenAPI-Specification/releases
-        $process = Process::run([
-            '/var/www/html/vendor/bin/openapi',
-            'app',
-            '-o',
-            'openapi.yaml',
-            '--version',
-            '3.1.0',
-        ]);
-        $error = $process->errorOutput();
-        $error = preg_replace('/^.*an object literal,.*$/m', '', $error);
-        $error = preg_replace('/^\h*\v+/m', '', $error);
-        echo $error;
-        echo $process->output();
-        // Convert YAML to JSON
-        $yaml = file_get_contents('openapi.yaml');
-        $json = json_encode(Yaml::parse($yaml), JSON_PRETTY_PRINT);
-        file_put_contents('openapi.json', $json);
-        echo "Converted OpenAPI YAML to JSON.\n";
-    }
-
-    public function init()
-    {
         // Generate APP_KEY if not exists
-
         if (empty(config('app.key'))) {
             echo "Generating APP_KEY.\n";
             Artisan::call('key:generate');
@@ -72,7 +30,7 @@ class Dev extends Command
         $settings = InstanceSettings::find(0);
         if (! $settings) {
             echo "Initializing instance, seeding database.\n";
-            Artisan::call('migrate --seed');
+            Artisan::call('migrate:fresh --seed');
         } else {
             echo "Instance already initialized.\n";
         }
